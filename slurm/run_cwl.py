f0b195f258e598d24cd0e896cec3d397c736ad5d
Shenglai Li <sli6@uchicago.edu>
2016-03-11 17:53:20 -0600

    fix(run): rm downloading ref

--- slurm/run_cwl.py
+++ slurm/run_cwl.py
@@ -96,7 +96,7 @@ if __name__ == "__main__":
     casedir = tempfile.mkdtemp(prefix="muse_%s_" %args.case_id, dir=args.basedir)
     workdir = tempfile.mkdtemp(prefix="workdir_", dir=casedir)
     inp = tempfile.mkdtemp(prefix="input_", dir=casedir)
-    index = tempfile.mkdtemp(prefix="index_", dir=casedir)
+    index = args.refdir
 
     #generate a random uuid
     vcf_uuid = uuid.uuid4()
@@ -121,7 +121,6 @@ if __name__ == "__main__":
 
     #download
     logger.info("getting refs")
-    pipelineUtil.download_from_cleversafe(logger, args.refdir, index, "/home/ubuntu/.s3cfg")
     reference_fasta_name = os.path.join(index,"GRCh38.d1.vd1.fa")
     reference_fasta_fai = os.path.join(index,"GRCh38.d1.vd1.fa.fai")
     dbsnp_known_snp_sites = os.path.join(index,"dbsnp_144.grch38.vcf.bgz")
