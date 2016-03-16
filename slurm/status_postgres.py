f5752b0846934158290fd94cfb4076826e530bbf
Shenglai Li <lyellooo@gmail.com>
2016-03-15 22:32:17 -0500

    fix(status): add primary key

--- slurm/status_postgres.py
+++ slurm/status_postgres.py
@@ -13,7 +13,8 @@ Base = declarative_base()
 
 class ToolTypeMixin(object):
     """ Gather information about processing status """
-
+    
+    id = Column(Integer, primary_key=True)
     case_id = Column(String)
     vcf_id = Column(String)
     files = Column(ARRAY(String))
